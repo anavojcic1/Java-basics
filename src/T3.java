90fe5e8e6e70741e973d4ad8f55361195c4fb398
anavojcic1 <123487786+anavojcic1@users.noreply.github.com>
2023-12-04 19:30:57 -0500

    Update T3.java

--- src/T3.java
+++ src/T3.java
@@ -1,6 +1,6 @@
-//Create a 2D array or integer type where you will store odd and even
-//numbers. Develop a program which will identify/print the even numbers
-//only.
+/*Create a 2D array or integer type where you will store odd and even
+numbers. Develop a program which will identify/print the even numbers
+only. */
 
 public class T3 {
     public static void main(String[] args) {
